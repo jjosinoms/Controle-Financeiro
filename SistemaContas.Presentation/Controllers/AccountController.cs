a4f44584a7719127278947195318e58869e977b1
Jonas <jjosinoms@gmail.com>
2023-01-25 13:35:33 -0300

    Camada 1 - Presentation | Login / Recuperar Senha / Cadastrar Usuario

---
+++ SistemaContas.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaContas.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+        //Account/PasswordRecover
+        public IActionResult PasswordRecover()
+        {
+            return View();
+        }
+    }
+}
