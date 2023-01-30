64df8d6c9f461b8058ee532a75974a0d0ab96ffd
Jonas <jjosinoms@gmail.com>
2023-01-30 13:04:23 -0300

    ViewModel OK / Cadastro de usuário OK, Integracao das camadas de projetos OK

--- SistemaContas.Presentation/Controllers/AccountController.cs
+++ SistemaContas.Presentation/Controllers/AccountController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaContas.Data.Entities;
+using SistemaContas.Data.Helpers;
+using SistemaContas.Data.Repositories;
+using SistemaContas.Presentation.Models;
+using System.Security.Cryptography;
 
 namespace SistemaContas.Presentation.Controllers
 {
@@ -9,13 +14,69 @@ namespace SistemaContas.Presentation.Controllers
         {
             return View();
         }
+        [HttpPost] // Recebe o SUBMIT do formulário
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
         //Account/Register
         public IActionResult Register()
         {
             return View();
         }
+        [HttpPost] // Recebe o SUBMIT do formulário
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Verificar se o email informado ja esta cadastrado no banco de dados
+                    var usuarioRepository = new UsuarioRepository();
+
+                    if (usuarioRepository.GetByEmail(model.Email) != null)
+                    {
+                        TempData["MensagemAlerta"] = "o email informado já está cadastrado no sistema, tente outro.";
+                    }
+                    else
+                    {
+
+                        //criar um objeto usuário
+                        var usuario = new Usuario();
+
+                        //capturar os dados do usuário enviados pelo formulário
+                        usuario.Id = Guid.NewGuid();
+                        usuario.Nome = model.Nome;
+                        usuario.Email = model.Email;
+                        usuario.Senha = MD5Helper.Encrypt(model.Senha);
+
+                        //gravando o usuario no banco de dados
+                        usuarioRepository.Add(usuario);
+
+                        TempData["MensagemSucesso"] = "Parabéns! Sua conta foi cadastrada com sucesso!";
+                        ModelState.Clear(); // limpar todos os campos do formulário
+                    }
+
+                }
+                catch(Exception e)
+                {
+                    TempData["MensagemErro"] = $"Falha ao cadastrar o usuário: {e.Message}.";
+                }
+                
+            }
+            else
+            {
+                TempData["MensagemAlerta"] = "Ocorreram erros de validação no prreenchimento do formulário.";
+            }
+            return View();
+        }
         //Account/PasswordRecover
         public IActionResult PasswordRecover()
+        {
+            return View();
+        }
+        [HttpPost] // Recebe o SUBMIT do formulário
+        public IActionResult PasswordRecover(PasswordRecoverViewModel model)
         {
             return View();
         }
---
+++ SistemaContas.Presentation/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaContas.Presentation.Models
+{
+    public class LoginViewModel
+    {
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe seu E-mail.")]
+        public string? Email { get; set; }
+
+        [MinLength(8, ErrorMessage = "Por favor, insira no mínimo {1} caracteres")]
+        [MaxLength(20, ErrorMessage = "Por favor, insira no máximo {1} caracteres")]
+        [Required(ErrorMessage = "Por favor, informe sua Senha.")]
+        public string? Senha { get; set; }
+
+    }
+}
