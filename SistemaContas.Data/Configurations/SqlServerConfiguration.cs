b0638fade4166eee92c65a7481dea8ecdab521cc
Jonas <jjosinoms@gmail.com>
2023-02-15 11:44:59 -0300

    criacao .gitignore e readme.md

--- SistemaContas.Data/Configurations/SqlServerConfiguration.cs
+++ SistemaContas.Data/Configurations/SqlServerConfiguration.cs
@@ -8,11 +8,9 @@ namespace SistemaContas.Data.Configurations
 {
     public class SqlServerConfiguration
     {
-        //    public static string ConnectionString
-        //      => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BD_SistemaContas;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
         public static string ConnectionString
-            => @"Data Source=SQL8004.site4now.net;Initial Catalog=db_a94bdf_bdsistemacontas;User Id=db_a94bdf_bdsistemacontas_admin;Password=j05JMS07";
+           => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BD_SistemaContas;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
 
     }
 }
